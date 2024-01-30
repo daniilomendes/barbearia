425e3db6a27d3656bae4fb018dc96b9ac43499fb
Danilo Mendes <danilo.mirandha@gmail.com>
2024-01-30 07:32:29 -0300

    feat: add search components

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,7 @@
 import { ptBR } from "date-fns/locale";
 import Header from "../_components/header";
-import { format } from 'date-fns'
+import { format } from "date-fns";
+import Search from "./_components/search";
 
 export default function Home() {
   return (
@@ -8,13 +9,17 @@ export default function Home() {
       <Header />
 
       <div className="px-5 pt-5">
-      <h2 className="text-xl font-bold">Olá, Danilo!</h2>
-      <p className=" capitalize text-sm">{format(new Date(), "EEEE',' dd 'de' MMMM", {
-        locale: ptBR
-      })}</p>
+        <h2 className="text-xl font-bold">Olá, Danilo!</h2>
+        <p className=" capitalize text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR,
+          })}
+        </p>
       </div>
 
-      
+      <div className="px-5 mt-6">
+        <Search />
+      </div>
     </div>
   );
 }
