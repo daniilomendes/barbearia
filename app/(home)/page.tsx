23952c7550f46132e5b2a8a5e128e7a92796e4e6
Danilo Mendes <danilo.mirandha@gmail.com>
2024-01-30 06:26:18 -0300

    feat: adicionando o header components na home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,9 @@
+import Header from "../_components/header";
+
 export default function Home() {
   return (
     <div>
-      HOME PAGE
+      <Header />
     </div>
   );
 }
---
+++ app/_components/header.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import { Card, CardContent } from "./ui/card";
+import { Button } from "./ui/button";
+import { MenuIcon } from "lucide-react";
+
+const Header = () => {
+  return (
+    <Card>
+      <CardContent className="p-5 justify-between flex flex-row items-center">
+        <Image src="/logo.png" alt="barbearia logo" height={18} width={120} />
+        <Button variant="outline" size="icon" className="h-8 w-8">
+          <MenuIcon size={16} />
+        </Button>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Header;
