74bd8c64aaac978c27dbde9ec06ad17e5e4ea8aa
Danilo Mendes <danilo.mirandha@gmail.com>
2024-01-31 20:18:19 -0300

    feat: add autenticação caso o usuário não autenticado tente fazer reservas sem estar logado

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,7 +1,8 @@
-
 import { db } from "@/app/lib/prisma";
 import BarbershopInfo from "./_components/barbershop-info";
 import ServiceItem from "./_components/service-item";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 interface BarbershopDetailsPageProps {
   params: {
@@ -12,19 +13,20 @@ interface BarbershopDetailsPageProps {
 const BarbershopDetailsPage = async ({
   params,
 }: BarbershopDetailsPageProps) => {
+  const session = await getServerSession(authOptions)
   if (!params.id) {
     // TODO: redirecionar para home page
     return null;
   }
 
   const barbershop = await db.barbershop.findUnique({
-    where:{
-      id: params.id
+    where: {
+      id: params.id,
     },
     include: {
-      services: true
-    }
-  })
+      services: true,
+    },
+  });
 
   if (!barbershop) {
     // TODO: redirecionar para home page
@@ -37,7 +39,7 @@ const BarbershopDetailsPage = async ({
 
       <div className="px-5 flex flex-col gap-4 py-6">
         {barbershop.services.map((service) => (
-          <ServiceItem key={service.id} service={service} />
+          <ServiceItem key={service.id} service={service} isAuthenticated={!!session?.user} />
         ))}
       </div>
     </div>
